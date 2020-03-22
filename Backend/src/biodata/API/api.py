d261330efa38f8728314d5cfe491ec7da464c258
hudanasution <hudanasution@gmail.com>
2020-03-22 14:16:55 +0700

    backend- adding biodata, biodatadetail and createbiodatastaff api

--- Backend/src/biodata/API/api.py
+++ Backend/src/biodata/API/api.py
@@ -1,23 +1,43 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 
-from rest_framework.generics import RetrieveAPIView,ListAPIView
+# from rest_framework.generics import RetrieveAPIView,ListAPIView
 
 from ..models import Biodata
-from .serializers import Biodataserializer, BiodataDetailserializer
+from .serializers import BiodataSerializer, BiodataDetailSerializer, CreateStaffBiodataSerializer
+from account.API.permission import IsAdmin
 import logging
 
-class GetBiodataAPI(ListAPIView):
+
+class GetBiodataAPI(generics.ListAPIView):
     permission_classes=[
-        # permissions.IsAuthenticated
-        permissions.AllowAny
+        permissions.IsAuthenticated, 
+        IsAdmin,
+        # permissions.AllowAny
     ]
-    serializer_class = Biodataserializer
+    serializer_class = BiodataSerializer
     queryset = Biodata.objects.all()
 
-class GetBiodataDetailAPI(RetrieveAPIView):
+class GetBiodataDetailAPI(generics.RetrieveAPIView):
+    permission_classes=[
+        permissions.AllowAny,
+    ]
+    serializer_class = BiodataDetailSerializer
+    queryset = Biodata.objects.all()
+
+# class CreateSiswaBiodataAPI():
+
+# class CreateAdminBiodataAPI():
+class CreateStaffBiodataAPI(generics.CreateAPIView):
     permission_classes=[
         permissions.AllowAny
     ]
-    serializer_class = BiodataDetailserializer
-    queryset = Biodata.objects.all()
+    serializer_class = CreateStaffBiodataSerializer
+
+    # def post(self, request, *args, **kwargs):
+    #     serializer=self.get_serializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     biodata=serializer.validated_data
+    #     return Response({
+    #         "biodata":CreateStaffBiodataSerializer(biodata,context=self.get_serializer_context()).data,
+    #     })
--- Backend/src/biodata/API/serializers.py
+++ Backend/src/biodata/API/serializers.py
@@ -1,13 +1,51 @@
 from rest_framework import serializers
 from ..models import Biodata
 
-#GET BIODATA DATA
-class Biodataserializer(serializers.ModelSerializer):
+class BiodataSerializer(serializers.ModelSerializer):
+    """
+    GET ALL BIODATA AVALIABLE
+    """
     class Meta:
         model = Biodata
         fields = "__all__"
 
-class BiodataDetailserializer(serializers.ModelSerializer):
+class BiodataDetailSerializer(serializers.ModelSerializer):
+    """
+    GET SPESIFIC BIODATA
+    """
     class Meta:
         model = Biodata
         fields = ('NomerInduk','Nama','Agama','TempatLahir','TanggalLahir','Alamat','NomerTLP','Email','PendidikanTerakhir','InstansiPendidikanTerakhir','Point','Status','Profilepicture')
+
+class CreateStaffBiodataSerializer(serializers.ModelSerializer):
+    """
+    CREATE STAFF BIODATA
+    """
+    staff_choices=[
+        ('Guru Aktif','Guru Aktif'),
+        ('Staf Aktif','Staf Aktif')
+    ]
+    class Meta:
+        model = Biodata
+        # fields = ['NomerInduk','Nama','TempatLahir','TanggalLahir','NomerTLP','Email']
+        fields = ['NomerInduk','Nama','Agama','TempatLahir','TanggalLahir','Alamat','NomerTLP','Email','PendidikanTerakhir','InstansiPendidikanTerakhir','Status','Profilepicture']
+        # fields = "__all__"
+        # def create(self, validated_data):
+        #     biodata = Biodata.objects.create_user(
+        #         NomerInduk = validated_data['NomerInduk'],
+        #         Nama = validated_data['Nama'],
+        #         TempatLahir = validated_data['TempatLahir'],
+        #         TanggalLahir = validated_data['TanggalLahir'],
+        #         Alamatt = validated_data['Alamat'],
+        #         NomerTLP = validated_data['NomerTLP'],
+        #         Email = validated_data['Email'],
+        #         Status = validated_data['Status'],
+        #     )
+            # biodata.save()
+            # return biodata
+
+
+# class CreateSiswaBiodataSerializer(serializers.ModelSerializer):
+#     """
+#     CREATE SISWA BIODATA
+#     """
