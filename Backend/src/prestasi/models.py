16d99049daa0bda06b919799499d7e7726eb623e
hudanasution <hudanasution@gmail.com>
2020-05-15 16:41:05 +0700

    backend-create instansi modal -add null to profile defualt value

--- Backend/src/prestasi/models.py
+++ Backend/src/prestasi/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Instansi(models.Model):
+    Nama_Instansi = models.CharField(max_length=60, unique=True)
+    Jenis_Instansi = models.CharField(max_length=30)
+    Keterangan_Instansi = models.TextField(max_length=240, null=True, blank=True)
+
+    def __str__(self):
+        return self.Nama_Instansi
