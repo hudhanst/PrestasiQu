2ee2c0479e68af86a7c4526a10e1c24aa6b7a7db
hudhanst <60328630+hudhanst@users.noreply.github.com>
2020-08-08 00:00:39 +0700

    backend-fix bug on createsuperuser -when firsttime create superuser a user need biodata  so need to create biodata function and run when createsuperuser

--- Backend/src/account/manager.py
+++ Backend/src/account/manager.py
@@ -1,16 +1,39 @@
 from django.db import models
+from biodata.models import Biodata
 from django.contrib.auth.models import BaseUserManager
+from datetime import datetime
+
 
 class UserManager(BaseUserManager):
+    def create_biodata(self, NomerInduk, Nama, TempatLahir, TanggalLahir):
+        if not NomerInduk:
+            raise ValueError('Users must have a nomerinduk ')
+        biodata = Biodata(
+            NomerInduk=NomerInduk,
+            Nama=Nama if Nama else NomerInduk,
+            TempatLahir=TempatLahir if TempatLahir else 'earth',
+            TanggalLahir=TanggalLahir if TanggalLahir else datetime.today(),
+        )
+
+        biodata.save(using=self._db)
+        return biodata
+
     def create_user(self, nomerinduk, password=None):
         """
         Creates and saves a User with the given email and password.
         """
         if not nomerinduk:
             raise ValueError('Users must have a nomerinduk ')
-
+        datenow = datetime.today()
+        biodata = self.create_biodata(
+            NomerInduk=nomerinduk,
+            Nama=nomerinduk,
+            TempatLahir=None,
+            TanggalLahir=datenow
+        )
         user = self.model(
             nomerinduk=nomerinduk,
+            profile=biodata
         )
 
         user.set_password(password)
